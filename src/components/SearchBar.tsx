093654340f50682cde38f8aa5e7a8f871ff40f7e
vasudravinarendran <vnarendran@ethz.ch>
2025-10-11 01:48:14 +0200

    fix

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,11 +1,6 @@
 import { useState } from "react";
-<<<<<<< HEAD
 import { HiOutlineMagnifyingGlass } from "react-icons/hi2";
 import styles from "./NavBar.module.css";
-=======
-import styles from "./NavBar.module.css";
-// import { HiOutlineMagnifyingGlass } from "react-icons/hi2";
->>>>>>> 8ae0a84ac7396fdc6955cf35b66636e1a389a479
 
 export default function SearchBar() {
   const [query, setQuery] = useState("");
