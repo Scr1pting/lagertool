a53a4e382e8b80bc65377954f39debd33ff92e57
EbrietanPhasmid <zawadiak.mrjz@gmail.com>
2025-10-10 21:05:30 +0200

    Ping prints

--- db/database.go
+++ db/database.go
@@ -1,7 +1,9 @@
 package db
 
 import (
+	"context"
 	"fmt"
+	"log"
 
 	"github.com/go-pg/pg/v10"
 )
@@ -24,8 +26,22 @@ func NewDBConn() (con *pg.DB, err error) {
 		PoolSize: 50,
 	}
 	con = pg.Connect(options)
-	if con == nil {
+
+	// Test connection to Postgres
+	ctx := context.Background()
+	if err := con.Ping(ctx); err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
 		return nil, err
 	}
+	fmt.Println("Connected to PostgreSQL database.")
 	return con, nil
+}
+
+func InitDB(con *pg.DB) {
+}
+
+func Close(con *pg.DB) {
+	if con != nil {
+		con.Close()
+	}
 }
