32c6ca0ac689ab0032dcf19f541bded23d3cafac
Jonas B <65017752+Scr1pting@users.noreply.github.com>
2025-10-10 17:58:15 +0200

    Update folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className={styles.header}>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
