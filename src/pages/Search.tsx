c124fad40889b5d70c770409e58a28783c1f80be
Jonas B <65017752+Scr1pting@users.noreply.github.com>
2025-10-11 01:00:11 +0200

    Add inventory list

---
+++ src/pages/Search.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import { useState, useEffect } from 'react';
+import DataTable from '@/components/DataTable';
+import type { ColumnDef } from '@tanstack/react-table';
+
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "shelf_name",
+    header: "Shelf",
+  },
+  {
+    accessorKey: "room_name",
+    header: "Room",
+  },
+  {
+    accessorKey: "building_name",
+    header: "Building",
+  },
+  {
+    accessorKey: "amount",
+    header: "Amount",
+  }  
+]
+
+
+function Search() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch("https://05.hackathon.ethz.ch/api/inventory")
+        .then((response) => response.json())
+        .then((json) => setData(json))
+        .catch((error) => console.error("Error fetching data:", error));
+    }, []);
+
+    return(
+        <div className="container mx-auto py-10">
+            <DataTable columns={columns} data={data} />
+        </div>
+    )
+}
+
+export default Search
