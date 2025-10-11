0bf5528a255853f4a703ce139cd77b6e4aac8d11
vasudravinarendran <vnarendran@ethz.ch>
2025-10-12 00:37:22 +0200

    fix navbar

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,13 @@
-import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Logo() {
   return (
-    <Routes>
-      <Route path="/" element={<img style={{"width": "40px", "marginRight": "20px", "cursor": "pointer"}} src='/public/logo.webp'/>} />
-    </Routes>
+    <Link to="/" aria-label="Home">
+      <img
+        style={{ width: "40px", marginRight: "20px", cursor: "pointer" }}
+        src="/logo.webp"
+        alt="Viscon logo"
+      />
+    </Link>
   );
 }
