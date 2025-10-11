b8c8b2c36529b4479fd4c1e53e5e8f0467e0d315
vasudravinarendran <vnarendran@ethz.ch>
2025-10-11 22:43:59 +0200

    add pages back in

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,10 @@ import EventsPage from './pages/EventsPage';
 import ItemDetailsPage from './pages/ItemDetailsPage';
 import LocationPage from './pages/LocationPage';
 import PersonHistoryPage from './pages/PersonHistoryPage';
+import InventoryStatusPage from './pages/InventoryStatusPage';
 import Search from './pages/Search';
 import ShelfBuilder from './pages/ShelfBuilder';
+import ShelvesPage from './pages/ShelvesPage';
 import Home from './pages/Home';
 import WithNavLayout from './components/WithNavBar';
 
@@ -40,9 +42,10 @@ function App() {
           <Route path="/persons" element={<AddPersonPage />} />
           <Route path="/persons/:personId" element={<PersonHistoryPage />} />
           <Route path="/items/:itemId" element={<ItemDetailsPage />} />
+          <Route path="/inventory" element={<InventoryStatusPage />} />
+          <Route path="/shelves" element={<ShelvesPage />} />
           <Route path="/events" element={<EventsPage />} />
         </Route>
-        
         <Route path="/shelf-builder" element={<ShelfBuilder />} />
       </Routes>
     </main>
